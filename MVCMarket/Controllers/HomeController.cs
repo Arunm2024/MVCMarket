8a4f545a448733014a129fd4106f2af8272de0fd
Arunm2024 <arunmehta1785@gmail.com>
2024-03-13 16:31:19 +0530

    Add project files.

---
+++ MVCMarket/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCMarket.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
