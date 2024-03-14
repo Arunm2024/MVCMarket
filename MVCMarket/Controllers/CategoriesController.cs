f8d4dee80b4d2abf5c344b93adaf2b5bbe976783
Arunm2024 <arunmehta1785@gmail.com>
2024-03-14 15:56:51 +0530

    files changes for section and validate of model properties

--- MVCMarket/Controllers/CategoriesController.cs
+++ MVCMarket/Controllers/CategoriesController.cs
@@ -7,11 +7,23 @@ namespace MVCMarket.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var category= CategoriesRepository.GetCategory();
+            return View(category);
         }
         public IActionResult Edit(int? id)
         {
-            var category= new Category() { CategoryId=id.HasValue? id.Value : 0 };
+            var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value:0);
+            //var category= new Category() { CategoryId=id.HasValue? id.Value : 0 };
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category) 
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
             return View(category);
         }
     }
--- MVCMarket/Program.cs
+++ MVCMarket/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+app.UseStaticFiles();
 app.UseRouting();
 app.MapControllerRoute(name:"default", 
     pattern:"{controller=Home}/{action=index}/{id?}");
